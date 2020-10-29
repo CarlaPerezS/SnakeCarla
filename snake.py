bba02c54328867cb984f631df8411a8ce32b107e
aranzanarvaez <73494800+aranzanarvaez@users.noreply.github.com>
2020-10-28 18:29:45 -0600

    Add files via upload
    La comida se mueve aleatoriamente y se queda dentro de la pantalla, además cuando el usuario pierde se muestra "Game Over"

---
+++ snake.py
@@ -0,0 +1,91 @@
+from turtle import *
+from random import randrange
+from freegames import square, vector
+import random
+
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+colors  = ["green","blue","orange","purple","pink","yellow"] 
+colorSnake = random.choice(colors)
+colorFood= random.choice(colors)
+if colorFood == colorSnake:
+    colorSnake = random.choice(colors)
+
+
+
+def change(x, y):
+    "Change snake direction."
+    aim.x = x
+    aim.y = y
+
+def inside(head):
+    "Return True if head inside boundaries."
+    return -200 < head.x < 190 and -200 < head.y < 190
+def insideFood(food):
+    "Return True if head inside boundaries."
+    return -200 < food.x < 190 and -200 < food.y < 190
+
+def move():
+    "Move snake forward one segment."
+    head = snake[-1].copy()
+    head.move(aim)
+    colors= ["black","purple","blue","green","gray"]
+    n= randrange(0,6)
+
+
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        write("Game Over", align="center", font=("Arial", 20, "bold"))
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9,colorSnake)
+    if body.x and body.y in snake == food.x and food.y:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+        
+    
+
+    square(food.x, food.y, 9,colorFood)
+    
+    update()
+    ontimer(move, 100)
+
+
+
+def moveFood():
+   
+    food.x =+ randrange(-10,11,10)
+    food.y =+ randrange(-10,11,10)
+    if not insideFood(food):
+        food.x = randrange(-15,15)*10
+        food.y= randrange(-15,15)*10
+    
+
+    ontimer(moveFood, 500)
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
+moveFood()
+done()
